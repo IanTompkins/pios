c8f09e4798f30c38022bac6e0f231362ae08ec85
IanTompkins <ian.tompkins@live.com>
2021-02-04 11:16:43 -0800

    Attempt to clear_bss with a for loop. Expereinceing warning with comparing pointer and integer.

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,6 +1,21 @@
 
 #include <stdio.h>
 
+extern long __bss_start;
+extern long __bss_end;
+
+//char *begin_bss = &__bss_start;
+//char *end_bss = &__bss_end;
+
+
+
+void clear_bss(){
+
+	for(int i = 0; i+(&__bss_start) < __bss_end; i++){
+		(&__bss_start)[i] = 0;
+	}
+}
+
 void kernel_main() {
 
     while(1){
