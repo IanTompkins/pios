325f4e29bef0ab19def40cc2e6148bd1dccbe966
IanTompkins <ian.tompkins@live.com>
2021-02-09 11:18:24 -0800

    Hopefully final commit. Need to understand '->' operations more.

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,23 +1,23 @@
 
 #include <stdio.h>
 
+int global;
+
 extern long __bss_start;
 extern long __bss_end;
 
-//char *begin_bss = &__bss_start;
-//char *end_bss = &__bss_end;
-
-
 
 void clear_bss(){
-
+	
+	char * bssStart = &__bss_start;
+	char * bssEnd = &__bss_end;
 	for(int i = 0; i+(&__bss_start) < __bss_end; i++){
 		(&__bss_start)[i] = 0;
 	}
 }
 
 void kernel_main() {
-
+	clear_bss();
     while(1){
     }
 }
